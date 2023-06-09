8a40c01788e52922762ff821a8341cb20fc2e4a5
abriz97 <andreabrizzi@live.it>
2023-06-09 17:41:56 +0100

    move table helpers

--- R/base_tables.R
+++ R/base_tables.R
@@ -32,6 +32,41 @@ table.na.to.empty <- function(DT, cols){
     return(table)
 }
 
+table.to.plot <- function(DT){
+    gridExtra::tableGrob(DT) |> gridExtra::grid.arrange()
+}
+
+prettify_cell <- function(..., parenthesis="(", precision=2, newline=FALSE, percent=FALSE, fmt_skeleton = NA_character_){
+
+    # writes M and CI expression in nice format ready to be put in a table.
+
+    n_vars <- enquos(...) |> length()
+
+    percent_sub <- fifelse(percent, yes='%%', no='') 
+    line_sub <- fifelse(newline, yes='\n', no=' ')
+    parenthesis_closed <- fcase(
+        parenthesis == "(", ")",
+        parenthesis == "[", "]",
+        parenthesis == "{", "}"
+    )
+
+    if(is.na(fmt_skeleton) ){
+        fmt_skeleton <- fcase(
+            n_vars == 1, "%._prec_fPERCENT",
+            n_vars == 2, "(%._prec_f - %._prec_f)",
+            n_vars == 3, "%._prec_fPERCENT_line_(%._prec_f - %._prec_f)"
+        )
+    }
+    fmt_skeleton <- gsub('_prec_',precision,fmt_skeleton)
+    fmt_skeleton <- gsub('PERCENT', percent_sub, fmt_skeleton)
+    fmt_skeleton <- gsub("\\(", parenthesis, fmt_skeleton)
+    fmt_skeleton <- gsub("\\)", parenthesis_closed, fmt_skeleton)
+    fmt_skeleton <- gsub("_line_", line_sub, fmt_skeleton)
+
+    sprintf(fmt_skeleton,...)
+}
+
+
 write.to.tex <- function(DT, file){
 
     # write to file the "body" of the table
@@ -47,7 +82,8 @@ write.to.tex <- function(DT, file){
     # however, we want to be able to quickly copy-paste the tabular env
     # so print a skeleton
     
-    skeleton <- xtable::xtable(DT[0], type='latex') %>% print(hline.after=c(), comment=FALSE) 
+    skeleton <- xtable::xtable(DT[0], type='latex') |>
+        print(hline.after=c(), comment=FALSE) 
 
     cmd_input <- sprintf("\n \\\\input{%s} \n \\\\end{tabular}", file.path("TODO", basename(file)))
     cmd <- sub('\\n \\\\end\\{tabular\\}', cmd_input, skeleton) 
